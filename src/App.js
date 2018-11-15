65a484c3fff5db24c4f5357af69d0b7ceb2612a6
Klemen Kunstek <klemen.kunstek1@gmail.com>
2018-11-15 12:05:57 +0100

    Add dependencies, implement UI & logic
    - Add google autocomplete
    - Add fetching local API for data
    - list retrieved data
    - Add google autocomplete and bootstrap dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Autocomplete from 'react-google-autocomplete';
+import { Table } from 'react-bootstrap';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+    };
+  }
+
+
+  getNearestPlaces(lat, lng) {
+    console.log(lat, lng);
+    try {
+      fetch(`http://localhost:10010/nearby?lat=${lat}&lng=${lng}`)
+        .then(response => response.json())
+        .then((data) => {
+          this.setState({ data: data.nearby_sites })
+          console.log(this.state);
+        }
+        );
+    } catch (error) {
+      console.log(error);
+      this.setState({ error: "Connection problem." })
+    }
+
+  }
+
   render() {
+    let lat = 0;
+    let lng = 0;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="jumbotron text-center">
+          <h2>Find closest Unesco sites</h2></div>
+        <div className="container">
+
+          <div style={{ display: 'block', textAlign: 'center' }}>
+            <Autocomplete
+              style={{ width: '50%' }}
+              onPlaceSelected={(place) => {
+                console.log("PLAC: ", place);
+
+                if (place.geometry) {
+                  lat = place.geometry.location.lat();
+                  lng = place.geometry.location.lng();
+                  this.getNearestPlaces(lat, lng);
+                } else {
+                  this.setState({ data: [] });
+                }
+              }}
+            />
+          </div>
+
+          <br /><br /><br />
+
+
+          {this.state.data.length > 0 &&
+            <Table striped bordered condensed hover>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Agent</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.data.map((place, i) => {
+                  return <tr key={i}>
+                    <td>{place.name}</td>
+                    <td>{place.agent}</td>
+                  </tr>
+                })}
+              </tbody>
+            </Table>}
+
+          {this.state.error && <div style={{ display: 'block', textAlign: 'center' }}>{this.state.error} </div>}
+        </div>
       </div>
     );
   }
